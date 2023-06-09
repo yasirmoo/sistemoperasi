65334379c9361b4281c1fcccc08a2490f45cc5a0
yasirmoo <yasirm000@gmail.com>
2023-06-09 20:53:28 +0700

    Menambahkan file Latihan1.bat ke respository

---
+++ latihan1.bat
@@ -0,0 +1,7 @@
+@echo off
+title Latihan 1 - Menampilkan Identitas Diri
+echo NIM                            : 20121097
+echo Nama                           : Yasir Arafat
+echo No Wa                          : 085237000250
+echo Alamat                         : Jl.Rambutan, griya Pasekaran, Blok E No 7
+pause
